e231b5bf4a1952400db58f841a8f6342e71b212c
luisrivas123 <luis.rivas@correounivalle.edu.co>
2022-06-15 22:16:00 -0500

    styled-components styles components GlobalStyles

---
+++ src/components/Category/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Anchor, Image } from './styles'
+
+const DEFAULT_IMAGE = 'https://imgur.com/dJa0Hpl.jpg'
+
+// Exportar nombrando los componentes
+// Props de Category
+export const Category = ({ cover = DEFAULT_IMAGE, path, emoji = '?' }) => (
+  <Anchor href={path}>
+    <Image src={cover} />
+    {emoji}
+  </Anchor>
+)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { createRoot } from "react-dom/client";
+// import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
-const container = document.getElementById('app');
-const root = createRoot(container);
+import { App } from './App'
+
+const container = document.getElementById('app')
+const root = createRoot(container)
 // root.render('Hola Luis');
-root.render(<h1>Hola Luis!!!!</h1>);
-// ReactDOM.render('Hola', document.getElementById('app'))
+root.render(<App />)
