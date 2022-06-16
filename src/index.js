0c33b7cc3544c7a956454af610ee0f98b8116564
luisrivas123 <luis.rivas@correounivalle.edu.co>
2022-06-15 20:45:31 -0500

    React React-dom babel linter vercel deploy

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Mi proyecto con React');
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+// root.render('Hola Luis');
+root.render(<h1>Hola Luis!!!!</h1>);
+// ReactDOM.render('Hola', document.getElementById('app'))
