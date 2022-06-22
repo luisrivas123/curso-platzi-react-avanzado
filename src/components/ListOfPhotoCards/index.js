79edec086c482b9f84c8929e5a322400b26a49aa
luisrivas123 <luis.rivas@correounivalle.edu.co>
2022-06-21 21:53:07 -0500

    HoC componente de orden superior

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,11 +1,48 @@
 import React from 'react'
+import { useQuery, gql } from "@apollo/client";
+
 import { PhotoCard } from '../PhotoCard'
 
+// Componente de orden superior, envuelve el componente
+// y recuperar la información 
+const whitPhotos = gql`
+  query getPhotos {
+    photos {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`;
+
+// export const ListOfPhotoCards = () => {
+//   return (
+//     <ul>
+//       {/* renderiza el PhotoCard */}
+//       {[1, 2, 3, 4, 5, 6, 7].map(id => <PhotoCard key={id} id={id} />)}
+//     </ul>
+//   )
+// }
+
 export const ListOfPhotoCards = () => {
+  const { loading, error, data } = useQuery(whitPhotos);
+
+  if (error) {
+    return <h2>Internal Server Error</h2>;
+  }
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
   return (
     <ul>
-      {/* renderiza el PhotoCard */}
-      {[1, 2, 3, 4, 5, 6, 7].map(id => <PhotoCard key={id} id={id} />)}
+      {data.photos.map((photo) => (
+        <PhotoCard key={photo.id} {...photo} />
+      ))}
     </ul>
-  )
-}
+  );
+};
+
