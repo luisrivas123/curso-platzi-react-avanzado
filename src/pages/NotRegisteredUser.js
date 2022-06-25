8d33df3f80ba8f52807aeb9a892aebe08087ec8a
luisrivas123 <luis.rivas@correounivalle.edu.co>
2022-06-24 20:17:31 -0500

    UseContest inicio de sesion

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,61 +1,67 @@
-import React, { Fragment } from 'react'
-import Context from '../Context'
+import React, { Fragment, useContext } from 'react'
+import { Context } from '../Context'
 
 import { UserForm } from '../components/UserForm'
 import { RegisterMutation } from '../container/RegisterMutation'
 import { LoginMutation } from '../container/LoginMutation'
 
 export const NotRegisteredUser = () => {
-
+  const { activateAuth } = useContext(Context);
   const { registerMutation, registerError, registerLoading } = RegisterMutation()
   const { loginMutation, loginError, loginLoading } = LoginMutation()
   // <h1>NotRegisteredUser</h1>
-  return(
   
-  <Context.Consumer>
-  {
-    ({ activateAuth }) => {
-      const onSubmitRegister = ({email, password}) => {
-        const input = { email, password }
-        const variables = { input }
-        registerMutation({ variables })
-        .then(activateAuth)
-      }
-      const onSubmitLogin = ({email, password}) => {
-        const input = { email, password }
-        const variables = { input }
-        loginMutation({ variables })
-        .then(activateAuth)
-      } 
-
-      const errorRegisterMsg = registerError && 'El usuario ya existe'
-      const erroLoginrMsg = loginError && 'El usuario o Contraseña invalidad'
-      // const errorMsg = registerError && 'El usuario ya existe'
-      // console.log(mutationLoading);
-      return (
-        <Fragment>
-
-          <UserForm 
-          disabled={registerLoading}
-          error={errorRegisterMsg} 
-          title='Registrarse' 
-          onSubmit={onSubmitRegister} />
-
-          <UserForm
-          disabled={loginLoading}
-          error={erroLoginrMsg} 
-          title='Iniciar sesión' 
-          onSubmit={onSubmitLogin} />
-
-        </Fragment>
-      )        
-      // return (
-      //   <form onSubmit={activateAuth}>
-      //     <button>Iniciar sesión</button>
-      //   </form>
-      // )
-    }
+  
+  const onSubmitRegister = ({email, password}) => {
+    const input = { email, password }
+    const variables = { input }
+    registerMutation({ variables })
+    // .then(activateAuth)
+    .then(({ data }) => {
+      const { signup } = data
+      // console.log(signup);
+      activateAuth(signup)
+    })
   }
-  </Context.Consumer>
-  )
+
+  const onSubmitLogin = ({email, password}) => {
+    const input = { email, password }
+    const variables = { input }
+    loginMutation({ variables })
+    // .then(response => {
+    //   console.log(response);
+    .then(({ data }) => {
+      const { login } = data
+      activateAuth(login)
+    })
+  } 
+
+  const errorRegisterMsg = registerError && 'El usuario ya existe'
+  const erroLoginrMsg = loginError && 'El usuario o Contraseña invalidad'
+  // const errorMsg = registerError && 'El usuario ya existe'
+  // console.log(mutationLoading);
+  return (
+    <Fragment>
+
+      <UserForm 
+      disabled={registerLoading}
+      error={errorRegisterMsg} 
+      title='Registrarse' 
+      onSubmit={onSubmitRegister} />
+
+      <UserForm
+      disabled={loginLoading}
+      error={erroLoginrMsg} 
+      title='Iniciar sesión' 
+      onSubmit={onSubmitLogin} />
+
+    </Fragment>
+  )        
+  // return (
+  //   <form onSubmit={activateAuth}>
+  //     <button>Iniciar sesión</button>
+  //   </form>
+  // )
+    
+  
 }
