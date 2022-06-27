c8bb31b5f9a4648bf352ce3b85edf01c171e3365
luisrivas123 <luis.rivas@correounivalle.edu.co>
2022-06-27 17:04:31 -0500

    Mejora Rutas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useContext } from 'react'
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
@@ -9,11 +9,12 @@ import { Detail } from './pages/Detail'
 import { User } from './pages/User'
 import { Favs } from './pages/Favs'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
+import { NotFound } from './pages/NotFound';
 
-import Context from './Context';
+import {Context} from './Context';
 
 export const App = () => {
-  
+  const { isAuth } = useContext(Context)
   return (
     <BrowserRouter>
       <div>
@@ -23,25 +24,48 @@ export const App = () => {
           <Route path='/' element={<Home />} />
           <Route path='/pet/:categoryId' element={<Home />} />
           <Route path='/detail/:detailId' element={<Detail />} />
+          <Route exact path='/favs' 
+            element={isAuth 
+              ? <Favs /> 
+              : <Navigate replace to='/login' />
+            } 
+          />
+          <Route exact path='/user' 
+            element={isAuth 
+              ? <User /> 
+              : <Navigate replace to='/login' />
+            } 
+          />
+          <Route exact path='/login' 
+            element={!isAuth 
+            ? <NotRegisteredUser /> 
+            : <Navigate replace to='/' />
+            } 
+          />
+          <Route path='*' element={<NotFound />} />
         </Routes>
-
-        <Context.Consumer>
-          {
-            ({ isAuth }) =>
-              isAuth
-                ? <Routes>
-                  <Route path='/user' element={<User />} />
-                  <Route path='/favs' element={<Favs />} />
-                </Routes>
-                : <Routes>
-                  <Route path='/user' element={<NotRegisteredUser />} />
-                  <Route path='/favs' element={<NotRegisteredUser />} />
-                </Routes>
-          }
-        </Context.Consumer>
-
         <NavBar />
       </div>
     </BrowserRouter>
   )
 }
+
+
+
+// return (
+//   <BrowserRouter>
+//     <GlobalStyle />
+//     <Logo />
+//     <Routes>
+//       <Route exact path='/' element={<Home />} />
+//       <Route exact path='/pet/:id' element={<Home />} />
+//       <Route exact path='/detail/:id' element={<Detail />} />
+//       <Route exact path='/favs' element={isAuth ? <Favorites /> : <Navigate replace to='/login' />} />
+//       <Route exact path='/user' element={isAuth ? <User /> : <Navigate replace to='/login' />} />
+//       <Route exact path='/login' element={!isAuth ? <NotRegisteredUser /> : <Navigate replace to='/' />} />
+//       <Route path='*' element={<NotFound />} />
+//     </Routes>
+//     <NavBar />
+//   </BrowserRouter>
+// )
+// }
